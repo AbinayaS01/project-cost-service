c0b532e253da82f183ed8532ba695951a2d385cf
Abhinav Sudhakar <28253243+AbhinavSdr@users.noreply.github.com>
2025-01-22 23:17:18 +0530

    initial upload

---
+++ src/main/java/com/ust/capstone/project_cost_service/ProjectCostServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ust.capstone.project_cost_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCostServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectCostServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ust/capstone/project_cost_service/ProjectCostServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ust.capstone.project_cost_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCostServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
